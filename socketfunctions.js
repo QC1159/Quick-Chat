a552ddd5663509f0c76f7ce5949b9847d8eb9d17
Jackson Haney <jacksonhaney0820@outlook.com>
2024-04-01 14:42:27 -0400

    Update Alpha 0.2

--- socketfunctions.js
+++ socketfunctions.js
@@ -2,6 +2,9 @@ let ws = "Closed"
 
 function startSocket() {
     ws = new WebSocket('wss://socketsbay.com/wss/v2/1/demo/');
+    ws.onclose = function() {
+        ws = new WebSocket('wss://socketsbay.com/wss/v2/1/demo/');
+    }
 }
 function send(msg, from, to) {
     ws.send("QUICKCHAT[{$msg=" + msg + ",$from=" + from + ",$to=" + to + "}]");
